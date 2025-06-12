07f1518ebbbf7772693c3e533bfa85a4bfc909f6
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2025-06-12 11:09:30 +0300

    minor change

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,8 +12,9 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-4 md:p-6 text-gray-900">
-                            {user.user_type} {user.name} Welcome!
+                             Welcome! {user.name} 
                         </div>
+                        
                     </div>
                 </div>
             </div>
